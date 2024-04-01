69979605e2fe6b3c2f3485c2f29bc35b6c6b26f0
Rishabh Mehta <144726868+rishabh20122003@users.noreply.github.com>
2024-04-01 21:30:32 +0530

    Add files via upload

---
+++ smallergreater.cpp.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//int n;
+	//cin>>a;
+	//cout<<"value of n is"<<n <<endl;
+	/*if(a>0)
+	{
+		cout<<"A is positive"<<endl;
+	}
+	else 
+	{
+		cout<<"A is negative"<<endl;
+		
+	}
+	*/
+	int a,b;
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+	cout<<"enter the value of b"<<endl;
+	cin>>b;
+	if(a>b)
+	{
+		cout<<"A is greater"<<endl;
+	}
+	if(b>a)
+	{
+		cout<<"B is greater"<<endl;
+	}
+}
