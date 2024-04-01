0ca7c0ff7748d9d729317653ad91911e2b00b179
Rishabh Mehta <144726868+rishabh20122003@users.noreply.github.com>
2024-04-01 21:31:25 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int a;
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+	
+	if(a>0)
+	{
+		cout<<"A is positive"<<endl;
+	}
+	else{if(a>0)
+	{
+		cout<<"A is negative";
+	}
+	else
+	{
+		cout<<"A is 0";
+	}
+	}}
+	
