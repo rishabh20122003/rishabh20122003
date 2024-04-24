1c9f5cb4e4376201030432a4afd73ca26820b7d1
Rishabh Mehta <144726868+rishabh20122003@users.noreply.github.com>
2024-04-24 23:19:34 +0530

    Add files via upload

---
+++ ifnested.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    
+    int a = 10; 
+    int b = 2; 
+    int c = 6; 
+  
+    
+    if (a < b) { 
+     
+        if (c < b) { 
+            printf("%d is the greatest", b); 
+        } 
+        else { 
+            printf("%d is the greatest", c); 
+        } 
+    } 
+    else { 
+      
+        if (c < a) { 
+            printf("%d is the greatest", a); 
+        } 
+        else { 
+            printf("%d is the greatest", c); 
+        } 
+    } 
+  
+    return 0; 
+}
